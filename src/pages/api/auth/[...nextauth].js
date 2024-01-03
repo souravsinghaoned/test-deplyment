a57c0564b4cbdf0ea562954e38f25b04f7d528dd
souravsinghaoned <souravsingha.one.directio@gmail.com>
2024-01-03 11:49:52 +0600

    added email provider

--- src/pages/api/auth/[...nextauth].js
+++ src/pages/api/auth/[...nextauth].js
@@ -1,16 +1,23 @@
 import EmailProvider from "next-auth/providers/email";
+import NextAuth from "next-auth"
+import GithubProvider from "next-auth/providers/github"
 
 
-providers: [
-    EmailProvider({
-      server: {
-        host: process.env.EMAIL_SERVER_HOST,
-        port: process.env.EMAIL_SERVER_PORT,
-        auth: {
-          user: process.env.EMAIL_SERVER_USER,
-          pass: process.env.EMAIL_SERVER_PASSWORD
-        }
-      },
-      from: process.env.EMAIL_FROM
-    }),
-  ]
+export const authOptions = {
+    // Configure one or more authentication providers
+
+    providers: [
+        EmailProvider({
+            server: {
+                host: process.env.EMAIL_SERVER_HOST,
+                port: process.env.EMAIL_SERVER_PORT,
+                auth: {
+                    user: process.env.EMAIL_SERVER_USER,
+                    pass: process.env.EMAIL_SERVER_PASSWORD
+                }
+            },
+            from: process.env.EMAIL_FROM
+        }),
+    ],
+}
+export default NextAuth(authOptions)
